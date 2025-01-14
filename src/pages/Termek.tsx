1ccc2571be965a0d926f1e488d6cb45c652c11a4
Keri Dominik <keridodorobi@gmail.com>
2025-01-14 11:22:14 +0100

    usestate

--- src/pages/Termek.tsx
+++ src/pages/Termek.tsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Termek = () => {
     const {id} = useParams();
-    return <h1>Termek ID: {id} </h1>
+    const[nev, setNev] = useState<number>(0);
+    return <h1>Termek ID: {id} {nev} </h1>
 };
 
 export default Termek;
