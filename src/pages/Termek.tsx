c8cef8daabca9af860a284f268e4c8cf235ca500
Keri Dominik <keridodorobi@gmail.com>
2025-01-14 11:27:39 +0100

    setValue

--- src/pages/Termek.tsx
+++ src/pages/Termek.tsx
@@ -1,10 +1,20 @@
-import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const Termek = () => {
-    const {id} = useParams();
-    const[nev, setNev] = useState<number>(0);
-    return <h1>Termek ID: {id} {nev} </h1>
+    const { id } = useParams();
+    const [nev, setNev] = useState<string>('');
+    return (
+        <>
+            <h1>Termék ID: {id}</h1>
+
+            <h1>Termék név: {nev}</h1>
+            <div>
+                <input type="text" onChange={(e) => setNev(e.target.value)}/>
+                <button>Start</button>
+            </div>
+        </>
+    );
 };
 
 export default Termek;
