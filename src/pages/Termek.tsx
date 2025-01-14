ce04d40f21f15e5728dea90576f1bfe21a8fd14e
Keri Dominik <keridodorobi@gmail.com>
2025-01-14 13:28:13 +0100

    Backend

--- src/pages/Termek.tsx
+++ src/pages/Termek.tsx
@@ -3,21 +3,10 @@ import { useNavigate, useParams } from 'react-router-dom';
 
 const Termek = () => {
     const { id } = useParams();
-    const navigate = useNavigate();
-    const [nev, setNev] = useState<string>('');
-    const submit = () => {
-        alert("Clicked")
-        navigate('/elso')
-    };
+   
     return (
         <>
-            <h1>Termék ID: {id}</h1>
-
-            <h1>Termék név: {nev}</h1>
-            <div>
-                <input type="text" onChange={(e) => setNev(e.target.value)}/>
-                <button onClick={submit}>Start</button>
-            </div>
+            
         </>
     );
 };
--- src/pages/Termekek.tsx
+++ src/pages/Termekek.tsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from 'react';
+import apiClient from '../api/apiClient';
+import {Termek} from '../types/Termek';
+
 const Termekek = () => {
+    const [data, setData] = useState<Array<Termek>>();
+    useEffect(() => {
+        apiClient
+            .get('/termekek')
+            .then((res) => {
+                console.log(res.data);
+            })
+            .catch((e) => console.error(e));
+    }, []);
+
     return (
         <>
             <h1>Termékek</h1>
+            <table>
+                <tbody>
+                    {data?.map((i: Termek)=> (<tr key={i.id}>
+                        <td>{i.nev}</td>
+                        <td>{i.ar}</td>
+                    </tr>))}
+                </tbody>
+            </table>
+            
         </>
     );
 };
